6b82667153813ddb0b41d5200e7554e1bc606f02
Justin Portell <72322829+jupo-ex-it@users.noreply.github.com>
2021-03-27 14:02:16 -0500

    mission complete

--- scripts.js
+++ scripts.js
@@ -1,2 +1,75 @@
-// Write your JavaScript code here.
-// Remember to pay attention to page loading!
+window.addEventListener("load", function () {
+    const takeoff = document.getElementById("takeoff");
+    const landing = document.getElementById("landing");
+    const missionAbort = document.getElementById("missionAbort");
+    const flightStatus = document.getElementById("flightStatus");
+    const up = document.getElementById("up");
+    const down = document.getElementById("down");
+    const right = document.getElementById("right");
+    const left = document.getElementById("left");
+    const rocket = document.getElementById("rocket")
+    let rocketPositionTop = 250;
+    let rocketPositionLeft = 0;
+
+    function rocketReset() {
+        rocketPositionTop = 250;
+        rocket.style.top = "250px";
+        rocketPositionLeft = 0;
+        rocket.style.left = "0px";
+    }
+
+    takeoff.addEventListener("click", function () {
+        window.confirm("Confirm that the shuttle is ready for takeoff.");
+        if (window.confirm) {
+            flightStatus.innerHTML = "Shuttle in flight.";
+            shuttleBackground.style.backgroundColor = "blue";
+            document.getElementById("spaceShuttleHeight").innerHTML = "10000";
+        };
+    })
+
+    landing.addEventListener("click", function () {
+        window.alert("The shuttle is landing. Landing gear engaged.");
+        flightStatus.innerHTML = "The shuttle has landed.";
+        shuttleBackground.style.backgroundColor = "green";
+        document.getElementById("spaceShuttleHeight").innerHTML = "0";
+        rocketReset()
+    })
+
+    missionAbort.addEventListener("click", function () {
+        window.confirm("Confirm that you want to abort the mission.");
+        if (window.confirm) {
+            flightStatus.innerHTML = "Mission aborted.";
+            shuttleBackground.style.backgroundColor = "green";
+            document.getElementById("spaceShuttleHeight").innerHTML = "0";
+            rocketReset()
+        };
+    })
+
+    up.addEventListener("click", function () {
+        if (rocketPositionTop > 0) {
+            rocketPositionTop -= 10;
+            rocket.style.top = rocketPositionTop + "px";
+        };
+    })
+
+    down.addEventListener("click", function () {
+        if (rocketPositionTop < 250) {
+            rocketPositionTop += 10;
+            rocket.style.top = rocketPositionTop + "px";
+        };
+    })
+
+    right.addEventListener("click", function () {
+        if (rocketPositionLeft < 360) {
+            rocketPositionLeft += 10;
+            rocket.style.left = rocketPositionLeft + "px";
+        };
+    })
+
+    left.addEventListener("click", function () {
+        if (rocketPositionLeft > -360) {
+            rocketPositionLeft -= 10;
+            rocket.style.left = rocketPositionLeft + "px";
+        };
+    })
+})
